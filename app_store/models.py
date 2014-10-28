c0d41467e8264fa6a491afc30d249afadee7acc6
Nate Mara <natemara@gmail.com>
2014-10-28 15:51:40 -0400

    add some basic models

--- app_store/models.py
+++ app_store/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class ForeignRepo(models.Model):
+	name = models.CharField(max_length=70)
+	url = models.TextField()
+
+
+class ForeignApplication(models.Model):
+	repository = models.ForeignKey(ForeignRepo)
+	application = models.ForeignKey(Application)
+	url = models.TextField()
+
+
+class Application(models.Model):
+	name = models.CharField(max_length=120)
