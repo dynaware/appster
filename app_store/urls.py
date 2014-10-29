9ea665346043b688e72b9a3d1cdff7e1f9efa9db
Nate Mara <natemara@gmail.com>
2014-10-29 09:20:05 -0400

    add url pattern for detailed view

--- app_store/urls.py
+++ app_store/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import patterns, url
 from app_store import views
 
 urlpatterns = patterns('',
-	url(r'^$', views.index, name='index')
+	url(r'^$', views.index, name='index'),
+	url(r'^(?P<app_id>\d+)/$', views.detail, name='detail'),
 )
