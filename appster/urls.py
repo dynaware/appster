b5eb59d55e04cb9c5889983ea9ed82f6d692af57
Nate Mara <natemara@gmail.com>
2014-10-29 09:17:22 -0400

    use app_store urls for root "/"

--- appster/urls.py
+++ appster/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+import app_store.urls
+
 urlpatterns = patterns('',
 	# Examples:
 	# url(r'^$', 'appster.views.home', name='home'),
 	# url(r'^blog/', include('blog.urls')),
 
 	url(r'^admin/', include(admin.site.urls)),
+	url(r'^$', include(app_store.urls)),
 )
