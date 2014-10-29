3b6fc46c3e3f5dbd6615b2f51b3b7339c5870ced
Nate Mara <natemara@gmail.com>
2014-10-29 09:02:23 -0400

    add a description field to the Application model

--- app_store/models.py
+++ app_store/models.py
@@ -18,11 +18,15 @@ class ForeignRepo(models.Model):
 			The url pattern that each application will have, the position of each app id
 			in the url will be replaced with the text "{APP_ID}".
 			(https://play.google.com/store/apps/details?id={APP_ID})
+
+		description:
+			The application's description
 	"""
 
 	name = models.CharField(max_length=70)
 	url = models.CharField(max_length=120)
 	app_url = models.CharField(max_length=120)
+	description = models.TextField()
 
 	def __str__(self):
 		return self.name
