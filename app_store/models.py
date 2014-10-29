469abcd76d3a41645153ab6b3b75f053e14907c1
Nate Mara <natemara@gmail.com>
2014-10-29 08:45:26 -0400

    add some comments to the ForeignRepo model

--- app_store/models.py
+++ app_store/models.py
@@ -2,6 +2,21 @@ from django.db import models
 
 
 class ForeignRepo(models.Model):
+	"""
+	Represents one instance of a foreign app repository (Google Play, Apple App Store, etc.)
+
+	Fields:
+		name   : The name of this repository
+			(Google Play Store)
+
+		url    : The main URL entry-point URL
+			(https://play.google.com)
+
+		app_url: The url pattern that each application will have, the position of each app id
+			in the url will be replaced with the text "{APP_ID}".
+			(https://play.google.com/store/apps/details?id={APP_ID})
+	"""
+
 	name = models.CharField(max_length=70)
 	url = models.CharField(max_length=120)
 	app_url = models.CharField(max_length=120)
