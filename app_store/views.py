1c1eacc27ff9ab421f3ce55adf29689a027f2cf2
Nate Mara <natemara@gmail.com>
2014-10-29 09:18:45 -0400

    create view for single app detail

--- app_store/views.py
+++ app_store/views.py
@@ -5,3 +5,7 @@ from app_store.models import *
 
 def index(request):
 	return render(request, 'app_store/index.html', {'apps': Application.objects.all()})
+
+
+def detail(request, app_id):
+	return render(request, 'app_store/detail.html', {'app': Application.objects.get(app_id)})
