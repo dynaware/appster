20deb9fe4aac14621a22223e95d6a2060d3d6cfe
Nate Mara <natemara@gmail.com>
2014-10-29 23:18:25 -0400

    set DEBUG to boolean

--- appster/settings.py
+++ appster/settings.py
@@ -23,7 +23,7 @@ SECRET_KEY = os.environ.get(
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', True)
+DEBUG = os.environ.get('DEBUG', 'TRUE') == 'TRUE'
 
 TEMPLATE_DEBUG = True
 
