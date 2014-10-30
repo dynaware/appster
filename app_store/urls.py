a7d399f73198d94a540419e8d0893a193e88a942
Nate Mara <natemara@gmail.com>
2014-10-30 10:04:46 -0400

    fix formatting of urlpatterns in app_store.urls

--- app_store/urls.py
+++ app_store/urls.py
@@ -2,7 +2,8 @@ from django.conf.urls import patterns, url
 
 from app_store import views
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+	'',
 	url(r'^$', views.index, name='index'),
 	url(r'^(?P<app_id>\d+)/$', views.detail, name='detail'),
 	url(r'^category/(?P<category_id>\d+)/$', views.category, name='category'),
