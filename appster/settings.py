bcfb027f2e4d123f6be54a175b8ce936fc27eb9b
Nate Mara <natemara@gmail.com>
2014-10-29 23:14:27 -0400

    fix debug environment variable name

--- appster/settings.py
+++ appster/settings.py
@@ -23,7 +23,7 @@ SECRET_KEY = os.environ.get(
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('PRODUCTION', True)
+DEBUG = os.environ.get('DEBUG', True)
 
 TEMPLATE_DEBUG = True
 
