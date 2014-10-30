740503194c346d4cb7b47f36143ba668eb648365
Nate Mara <natemara@gmail.com>
2014-10-30 10:12:17 -0400

    add static file handler for non-develop environments

--- appster/urls.py
+++ appster/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 import app_store.urls
+from appster import settings
 
 urlpatterns = patterns(
 	'',
@@ -9,3 +10,9 @@ urlpatterns = patterns(
 	url(r'^app_store/', include(app_store.urls)),
 	url(r'^$', include(app_store.urls)),
 )
+
+if not settings.DEBUG:
+	urlpatterns += patterns(
+		'',
+		(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+	)
