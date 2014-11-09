393a78c29abab4bf72d0271a83358204e57150fe
Nate Mara <natemara@gmail.com>
2014-11-09 11:05:56 -0500

    add description field to category model closes #28

--- app_store/models.py
+++ app_store/models.py
@@ -11,6 +11,7 @@ class Category(models.Model):
 			(games, books, utilities)
 	"""
 	title = models.CharField(max_length=20)
+	description = models.TextField(null=True)
 
 	def __str__(self):
 		return self.title
