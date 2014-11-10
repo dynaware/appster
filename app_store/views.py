87894185553e96111a39fdd3669fe349b5b4fc31
Nate Mara <natemara@gmail.com>
2014-11-10 16:19:20 -0500

    display related apps in bottom of detail page closes #29

--- app_store/views.py
+++ app_store/views.py
@@ -16,7 +16,22 @@ def index(request):
 
 def detail(request, app_id):
 	app = Application.objects.get(id=app_id)
-	return render(request, 'app_store/detail.html', {'app': app, 'title': '{} | Appster'.format(app.name)})
+	return render(
+		request,
+		'app_store/detail.html',
+		{
+			'app': app,
+			'title': '{} | Appster'.format(app.name),
+			'related_apps': sorted(
+				Application.objects.filter(
+					category=app.category.id,
+				).exclude(
+					id = app.id,
+				)[:4],
+				key=lambda x: x.rating, reverse=True
+			),
+		}
+	)
 
 
 def category(request, category_id):
