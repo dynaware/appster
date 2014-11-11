4c8a9c870fd4589979a6550f172b4d8165293d36
Nate Mara <natemara@gmail.com>
2014-11-11 10:15:00 -0500

    change app detail url

--- app_store/urls.py
+++ app_store/urls.py
@@ -5,7 +5,7 @@ from app_store import views
 urlpatterns = patterns(
 	'',
 	url(r'^$', views.index, name='index'),
-	url(r'^(?P<app_id>\d+)/$', views.detail, name='detail'),
+	url(r'^app/(?P<app_id>\d+)/$', views.detail, name='detail'),
 	url(r'^category/(?P<category_id>\d+)/$', views.category, name='category'),
 	url(r'^categories$', views.categories, name='categories'),
 	url(r'^search$', views.search, name='search'),
