8254941b38bcd5abdd65c546acfc4c206d00a084
Nate Mara <natemara@gmail.com>
2014-12-04 11:14:34 -0500

    add tests for page rendering

--- app_store/tests.py
+++ app_store/tests.py
@@ -1,4 +1,6 @@
 from django.test import TestCase, Client
+from django.core.urlresolvers import reverse
+
 from app_store.models import *
 
 
@@ -54,3 +56,56 @@ class TestApplicationModel(TestCase):
 			c.get(url)
 
 		self.assertEqual(dropbox().click_count, 100)
+
+
+class TestPageRendering(TestCase):
+	def setUp(self):
+		self.productivity = Category.objects.create(title='Productivity')
+		self.dropbox = Application.objects.create(
+			name='Dropbox',
+			description='',
+			category=self.productivity,
+		)
+		self.app_list = ApplicationList.objects.create(
+			name='My favorite apps',
+			image='http://fromthebungalow.files.wordpress.com/2011/05/thumbs-up.jpg',
+		)
+
+		self.client = Client()
+
+	def load_one_page(self, url, status=200):
+		response = self.client.get(url)
+		self.assertEqual(response.status_code, status)
+
+	def test_index_page(self):
+		self.load_one_page('/')
+
+	def test_detail_page(self):
+		self.load_one_page(reverse('detail', args=(self.dropbox.id,)))
+
+	def test_category_page(self):
+		self.load_one_page(reverse('category', args=(self.productivity.id,)))
+
+	def test_categories_page(self):
+		self.load_one_page(reverse('categories'))
+
+	def test_search_page(self):
+		self.load_one_page(reverse('search'))
+
+	def test_app_lists_page(self):
+		self.load_one_page(reverse('app_lists'))
+
+	def test_app_list_page(self):
+		self.load_one_page(reverse('app_list', args=(self.app_list.id,)))
+
+	def test_review_page(self):
+		self.load_one_page(reverse('new_review', args=(self.dropbox.id,)), status=302)
+
+	def test_new_app_page(self):
+		self.load_one_page(reverse('new_app'), status=302)
+
+	def test_review_apps_page(self):
+		self.load_one_page(reverse('review_apps'), status=302)
+
+	def test_about_page(self):
+		self.load_one_page(reverse('about'))
